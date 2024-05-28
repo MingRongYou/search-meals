2d13aba48d5334b30ef8d74299411241e645e632
MingRongYou <namova26@gmail.com>
2024-05-28 23:50:59 +0800

    feat: 🎸 add axios letter and ingredient

--- src/views/MealsByLetter.vue
+++ src/views/MealsByLetter.vue
@@ -8,7 +8,22 @@
       {{letter}}
     </router-link>
   </div>
+  <div>
+    <pre>
+      {{ meals }}
+    </pre>
+  </div>
 </template>
 <script setup>
+import { computed, onMounted } from "vue";
+import { useRoute } from "vue-router";
+import store from "../store";
+
+const route = useRoute();
 const letters = 'ABCDFGHIJKLMNOPQRSTUVWXYZ'.split('');
+const meals = computed(()=>store.state.mealsByLetter);
+
+onMounted(()=>{
+  store.dispatch('searchMealsByLetter',route.params.letter)
+});
 </script>
